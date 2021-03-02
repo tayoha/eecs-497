adecc619dbb6d00bb60333b1cdfb7a6dac905002
utsavlathia <utlathia@umich.edu>
2021-03-02 12:47:10 -0500

    api works for any search term

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ myHeading.textContent = 'This js text replaced the old html text';
 const myImg = document.querySelector('img');
 
 var API_KEY = '20169026-d45bc99749bd521df7aa7b5f4';
-var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('green snakes');
+var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('chocolate milkshake');
 $.getJSON(URL, function(data){
 if (parseInt(data.totalHits) > 0)
     $.each(data.hits, function(i, hit){ 
