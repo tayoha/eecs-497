04d0802ed6a57373594e747dbbf608277afb3c63
utsavlathia <utlathia@umich.edu>
2021-03-02 12:45:00 -0500

    high quality image from webFormatURL used

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ myHeading.textContent = 'This js text replaced the old html text';
 const myImg = document.querySelector('img');
 
 var API_KEY = '20169026-d45bc99749bd521df7aa7b5f4';
-var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('apples');
+var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('green snakes');
 $.getJSON(URL, function(data){
 if (parseInt(data.totalHits) > 0)
     $.each(data.hits, function(i, hit){ 
-        console.log(hit.pageURL); 
-        myImg.src = hit.pageURL
+        console.log(hit.webformatURL); 
+        myImg.src = hit.webformatURL
         return false;
     });
 else
