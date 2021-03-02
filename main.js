23bdd488ff5b23ba953527045e9c43cd042b3101
utsavlathia <utlathia@umich.edu>
2021-03-02 12:34:47 -0500

    geting a cross origin error when I try and replace the img source

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'This js text replaced the old html text';
+const myImg = document.querySelector('img');
 
 var API_KEY = '20169026-d45bc99749bd521df7aa7b5f4';
 var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('apples');
 $.getJSON(URL, function(data){
 if (parseInt(data.totalHits) > 0)
-    $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+    $.each(data.hits, function(i, hit){ 
+        console.log(hit.pageURL); 
+        myImg.src = hit.pageURL
+        return false;
+    });
 else
     console.log('No hits');
 });
